2d23b0ed46cbb61d66e8503886dd17099e091f04
Alan <dev.alancr@gmail.com>
2023-11-19 04:06:53 -0600

    Cambios

--- proyecto.cpp
+++ proyecto.cpp
@@ -1,5 +1,5 @@
-// Cordero Ramirez Alan
-// Plata Oropeza Raul David
+// Alan
+// Raul
 // POO 3-5
 // Proyecto
 
@@ -32,7 +32,7 @@
 #define INSTRUCCIONES "Mover cursor: Arriba / Abajo | Entrar: Enter | Cambiar P\xA0g: Izquierda / Derecha"
 
 using namespace std;
-
+void abrirMenu(int menu, int submenu);
 void gotoxy(int x, int y)
 { // Esta funcion nos permite escribir texto en cualquier parte de la consola
     HANDLE ventana;
@@ -64,6 +64,7 @@ class Menu
         }
         void dibujarCuadrado()
         { // Este cuadrado será el principal canvas del programa
+            system("cls");
             gotoxy(25, 1);
             cout << LINEA;
             for(int i=2; i<27; i++)
@@ -78,34 +79,74 @@ class Menu
             gotoxy(POSICION_X-7, 28);
             cout << INSTRUCCIONES;
         }
-        void obtenerTexto(int menu, int submenu)
+        int obtenerTexto(int menu, int submenu)
         { // En este metodo obtendremos los textos necesarios para cada menu y submenu
             switch(menu)
             {
-            case 0:
+            case 1:
+                switch(submenu)
+                {
+                    case 3:
+                        opciones.push_back("Nombre");
+                        opciones.push_back("Talla");
+                        opciones.push_back("Color");
+                        opciones.push_back("Cantidad");
+                        opciones.push_back("Precio");
+                        opciones.push_back("Confirmar");
+                        numOpciones = 6;
+                        break;
+
+                    case 10:
+                        opciones.push_back("Ventas");
+                        opciones.push_back("Buscar producto");
+                        opciones.push_back("Devoluciones");
+                        opciones.push_back("Inventario");
+                        numOpciones = 4;
+                        break;
+                }
                 break;
 
-            case 1:
-                opciones.push_back("Ingrese el nombre del empleado");
+            case 2:
+                switch(submenu)
+                {
+                    case 1:
+                        break;
 
-            case 6:
+                    case 10:
+                        opciones.push_back("Baja productos");
+                        opciones.push_back("Alta empleados");
+                        opciones.push_back("Baja empleados");
+                        opciones.push_back("Tickets");
+                        numOpciones = 4;
+                        break;
+                }
+                break;
+
+            case 3:
                 opciones.push_back("Usuario");
                 opciones.push_back("Contrase\xA4""a");
                 opciones.push_back("Ingresar");
-                opciones.push_back("Salir");
-                numOpciones = 4;
+                numOpciones = 3;
+                break;
+
+            case 4:
+                opciones.push_back("NO");
+                opciones.push_back("S\xD6");
+                return 1;
                 break;
             
             default:
                 break;
             }
+            opciones.push_back("Salir");
+            return numOpciones;
         }
         void imprimirTexto(int posicion_flecha)
         { // Se imprimen los textos obtenidos en la funcion obtenerTexto
             y = POSICION_Y;
             gotoxy(POSICION_TITULO+margenTitulo, 2);
             cout << nombreMenu;
-            for(int i=0; i<numOpciones; i++)
+            for(int i=0; i<=numOpciones; i++)
             {
                 gotoxy(POSICION_X, y);
                 if(i == posicion_flecha) cout << opciones[i] << " <---";
@@ -113,18 +154,18 @@ class Menu
                 y += 3;
             }
         }
-        int cambiarPosicion(int tecla, int posicion, int maxPosicion)
+        int cambiarPosicion(int tecla, int posicion)
         { // Metodo que retorna la posicion del cursor
             switch (tecla)
             {
             case ARRIBA:
                 posicion--;
-                if(posicion < 0) posicion = maxPosicion-1;
+                if(posicion < 0) posicion = numOpciones;
                 break;
             
             case ABAJO:
                 posicion++;
-                if(posicion == maxPosicion) posicion = 0;
+                if(posicion > numOpciones) posicion = 0;
                 break;
 
             default:
@@ -132,55 +173,45 @@ class Menu
             }
             return posicion;
         }
+        int confirmar()
+        {
+            const int confirmar = obtenerTexto(4, 0);
+            int posicion_conf = 0;
+            int tecla_conf;
+            while(1)
+            {
+                dibujarCuadrado();
+                imprimirTexto(posicion_conf);
+                tecla_conf = getch();
+                posicion_conf = cambiarPosicion(tecla_conf, posicion_conf);
+                if(tecla_conf == ENTER)
+                {
+                    if(posicion_conf) return 1;
+                    else return 0;
+                }
+            }
+        }
 };
 
 class Zapato
 { // Clase para identificar los zapatos a vender
     private:
-        string modelo;
-        string color;
+        string modelo, color;
         long int numero_serie;
-        int talla;
+        int talla, cantidad;
         float precio;
 
     public:
-        Zapato(void) {}
-        void obtenerDeArchivo()
+        Zapato(string _modelo, string _color, int _talla, int _cantidad, float _precio)
         {
-
+            modelo = _modelo;
+            color = _color;
+            talla = _talla;
+            cantidad = _cantidad;
+            precio = _precio;
         }
-        void alta()
+        void obtenerDeArchivo(int nZapato)
         {
-            
-        }
-        void reestock()
-        {
-
-        }
-        void buscar()
-        {
-
-        }
-};
-
-class Empleado
-{
-    private:
-        string nombre;
-        string password;
-        int codigo, salario, tipoEmpleado;
-        vector <string> datosEmpleado;
-
-    public:
-        Empleado(string _nombre, string _password, int _salario, int _tipoEmpledo)
-        {
-            nombre = _nombre;
-            password = _password;
-            salario = _salario;
-            tipoEmpleado = _tipoEmpledo;
-        }
-        void recuperarDeArchivo(int usuario)
-        { // AHORA ESTA FUNCION SOLO NOS VA A AYUDAR A RECUPERAR DATOS DEL ARCHIVO, PARA AHORRAR CODIGO EN LAS OTRAS FUNCIONES
             fstream fin("empleados.csv", ios::in);
             string temp, linea, dato;
             while(fin.eof() == 0)
@@ -202,7 +233,65 @@ class Empleado
             fin.close();
         }
         void alta()
-        { // HARD CODEADO
+        {
+            recuperarDeArchivo(0);
+            fstream fout;
+            fout.open("zapatos.csv", ios::out | ios::app);
+            fout << codigo++ << ", " << modelo << ", " << color << ", " << talla << ", " << cantidad << ", " << precio <<"\n";
+            fout.close();
+        }
+        void reestock()
+        {
+
+        }
+        void buscar()
+        {
+
+        }
+};
+
+class Empleado
+{
+    private:
+        string nombre, password;
+        int codigo, tipoEmpleado;
+        float salario;
+        char genero;
+        int horario[7];
+        vector <string> datosEmpleado;
+
+    public:
+        Empleado(string _nombre, string _password, int _salario, int _tipoEmpledo)
+        {
+            nombre = _nombre;
+            password = _password;
+            salario = _salario;
+            tipoEmpleado = _tipoEmpledo;
+        }
+        void recuperarDeArchivo(int usuario)
+        { // Esta funcion nos ayuda a recuperar datos del archivo y reutilizarlos a lo largo del objeto
+            fstream fin("empleados.csv", ios::in);
+            string temp, linea, dato;
+            while(fin.eof() == 0)
+            {
+                datosEmpleado.clear();
+                getline(fin, linea);
+                stringstream s(linea);
+                while(getline(s, dato, ','))
+                {
+                    datosEmpleado.push_back(dato);
+                }
+                codigo = stoi(datosEmpleado[0]);
+                if(codigo == usuario)
+                {
+                    fin.close();
+                    break;
+                }
+            }
+            fin.close();
+        }
+        void alta()
+        {
             recuperarDeArchivo(0);
             fstream fout;
             fout.open("empleados.csv", ios::out | ios::app);
@@ -224,7 +313,6 @@ class Empleado
             comparar = pass.compare(password);
             if(usuario == codigo && comparar == 0)
             {
-                cout << "LOGRADO";
                 Sleep(1000);
                 return stoi(datosEmpleado[4]);
             }
@@ -241,53 +329,173 @@ class Empleado
 };
 
 int buscar()
-{ // TODA LA FUNCION ESTA HARD CODEADA
+{
     Menu buscar = Menu("Buscar Empleados", 0);
     int codigo;
     int posicion = 0;
     while(1)
     {
-        system("cls");
         buscar.dibujarCuadrado();
-        //buscar.obtenerTexto(1);
-        //buscar.imprimirTexto(posicion); POR ALGUNA PERRA RAZON EL PROGRAMA CRASHEBA SI DEJO ESAS 2 LINEAS
-        gotoxy(POSICION_X, 2);
-        cout << "Ingrese codigo de empleado";
-        gotoxy(POSICION_X, 5);
-        cin >> codigo;
-        system("cls");
-        buscar.dibujarCuadrado();
-        Empleado empleado = Empleado("temp", "temp", 0, 0);
-        getch();
-        return 0;
     }
 }
 
-void menuOpciones(string titulo, int margen, int ID_menu, int submenu)
+int altasProductos(int menu, int submenu, int margen)
 {
-    Menu menuTexo = Menu(titulo, margen);
+    Menu altas = Menu("Altas productos", margen);
+    string nombre = "", color = "";
+    int talla = 0, cantidad = 0, tecla = 0, posicion = 0;
+    float precio = 0;
+    const int salir = altas.obtenerTexto(menu, submenu);
+    while(1)
+    {
+        altas.dibujarCuadrado();
+        altas.imprimirTexto(posicion);
+        gotoxy(POSICION_X, 5);
+        cout << nombre;
+        gotoxy(POSICION_X, 8);
+        cout << talla;
+        gotoxy(POSICION_X, 11);
+        cout << color;
+        gotoxy(POSICION_X, 14);
+        cout << cantidad;
+        gotoxy(POSICION_X, 17);
+        cout << precio;
+        tecla = getch();
+        posicion = altas.cambiarPosicion(tecla, posicion);
+        if(tecla == ENTER)
+        {
+            if(posicion == salir)
+            {
+                altas.~Menu();
+                return 0;
+            }
+            switch (posicion)
+            {
+            case 0:
+                gotoxy(POSICION_X, 5);
+                cout << ESPACIO;
+                gotoxy(POSICION_X, 5);
+                cin >> nombre;
+                break;
+
+            case 1:
+                gotoxy(POSICION_X, 8);
+                cout << ESPACIO;
+                gotoxy(POSICION_X, 8);
+                cin >> talla;
+                break;
+
+            case 2:
+                gotoxy(POSICION_X, 11);
+                cout << ESPACIO;
+                gotoxy(POSICION_X, 11);
+                cin >> color;
+                break;
+
+            case 3:
+                gotoxy(POSICION_X, 14);
+                cout << ESPACIO;
+                gotoxy(POSICION_X, 14);
+                cin >> cantidad;
+                break;
+
+            case 4:
+                gotoxy(POSICION_X, 17);
+                cout << ESPACIO;
+                gotoxy(POSICION_X, 17);
+                cin >> precio;
+                break;
+
+            case 5:
+                if(nombre.compare("") == 0 || color.compare("") == 0) cout << "Por favor rellena los campos";
+                else
+                {
+                    if(altas.confirmar())
+                    {
+                        Zapato altaZapato = Zapato(nombre, color, talla, cantidad, precio);
+                        altaZapato.alta()
+                    }
+                }
+                break;
+
+            default:
+                break;
+            }
+
+        }
+    }
+
+}
+
+int menuOpciones(string titulo, int margen, int ID_menu, int submenu)
+{
+    Menu menuTexto = Menu(titulo, margen);
     int posicion = 0;
+    const int salir = menuTexto.obtenerTexto(ID_menu, submenu);
     int tecla;
+    while(1)
+    {
+        menuTexto.dibujarCuadrado();
+        menuTexto.imprimirTexto(posicion);
+        tecla = getch();
+        posicion = menuTexto.cambiarPosicion(tecla, posicion);
+        if(tecla == ENTER)
+        {
+            if(posicion == salir)
+            {
+                menuTexto.~Menu();
+                return 0;
+            }
+            abrirMenu(ID_menu, posicion);
+        }
+    }
 }
 
 void abrirMenu(int menu, int submenu)
 { // Funcion que abre los menus y submenus
     switch(menu)
     {
-    case 0:
-        break;
-
     case 1:
+        switch(submenu)
+        {
+            case 0:
+                break;
+
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                
+                break;
+
+            case 10:
+                menuOpciones("Men\xA3 principal", -5, menu, submenu);
+                break;
+        }
         break;
-    
+
     case 2:
-        break;
+        switch(submenu)
+        {
+            case 0:
+                break;
 
-    case 3:
-        break;
+            case 1:
+                break;
 
-    case 4:
-        menuOpciones("Men\xA3 principal", -5, menu, submenu);
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 10:
+                menuOpciones("Men\xA3 de administradores", -5, menu, submenu);
+                break;
+        }
         break;
 
     default:
@@ -302,10 +510,9 @@ int login()
     int codigo = 0;
     int posicion = 0;
     int tecla, iniciarMenu;
+    int salir = login.obtenerTexto(3, 0);
     while(1)
     {
-        system("cls");
-        login.obtenerTexto(6, 0);
         login.dibujarCuadrado();
         login.imprimirTexto(posicion);
         gotoxy(POSICION_X, 5);
@@ -313,9 +520,10 @@ int login()
         gotoxy(POSICION_X, 8);
         cout << password;
         tecla = getch();
-        posicion = login.cambiarPosicion(tecla, posicion, 4);
+        posicion = login.cambiarPosicion(tecla, posicion);
         if(tecla == ENTER)
         {
+            if(posicion == salir) return 0;
             switch(posicion)
             {
                 case 0:
@@ -334,65 +542,25 @@ int login()
 
                 case 2:
                 {
-                    Empleado empleado = Empleado("temp", password, 0, 0); // CASE HARD CODEADO
+                    Empleado empleado = Empleado("temp", password, 0, 0);
                     iniciarMenu = empleado.comprobarPass(codigo);
-                    if(iniciarMenu == 0) break;
                     empleado.~Empleado();
-                    switch (iniciarMenu)
-                    {
-                        case 1:
-                            break;
-
-                        case 2:
-                            break;
-                        
-                        case 3:
-                            abrirMenu(4, 0);
-                            break;
-
-                        default:
-                            break;
-                    }
+                    if(iniciarMenu == 0) break;
+                    abrirMenu(iniciarMenu, 10);
                     codigo = 0;
                     password = "";
+                    break;
                 }
-
-                case 3:
-                    return 0;
 
                 default:
                     break;
             }
         }
-
     }
 }
 
 int main()
 {
-    // Empleado empleado = Empleado("Admin", "1234", 900, 3);
-    // empleado.alta();
-    login();
+    altasProductos(1, 3, -4);
+    // login();
 }
-
-// ------------------------------------------------------------------------------
-
-// BASURA QUE HACE FALTA (POR AHORA)
-
-/*
---------------------------------------------------------------------- 1era revisión
-    > GUARDAR Y RECOLECTAR LOS DATOS DE LOS CHAMBEADORES EN UN ARCHIVO DE TEXTO PARA EL LOGIN (YA)
-    > TERMINAR EL LOGIN Y SUS OPCIONES
-    > PONER EN PRACTICA LA CLASE DE LOS MENUS
-    > AL CHILE NI SE QUE TANTO LE VAMOS A METER AL PROYECTO
-
---------------------------------------------------------------------- 2da revisión
-    > YA FUNCIONA EL LOGIN CON EL ADMIN, YA FALTARIA IMPLEMENTARLO EN LOS DEMAS EMPLEADOS
-    > LA FUNCION ALTAS DE LOS EMPLEADOS POR AHORA ESTA HARD CODEADA, LUEGO LO CAMBIO YA QUE HAGAMOS LAS ALTAS
-    > YA SE CREA EL ARCHIVO, Y LO LEE, PUTO DESMADRE HACER ESA MAMADA
-
---------------------------------------------------------------------- 3ra revisión
-    > AHORA USAMOS UN GITHUB WUUUUUUUUUUUUUU
-    > POR FAVOR APRENDE A USAR GIT QUE YA NO TE VOY A PASAR ARCHIVOS POR WHATS
-    > PORFA SI QUIERA DIME QUE COSAS VAMOS A METERLE AL PROYECTO PARA TENER CLARO LO QUE VOY A PROGRAMAR
-*/
